f66a8ebf28597122dd9706ac23338e57d0c80189
Nick Astroth <nick.astroth@torussphere.com>
2016-11-03 11:50:55 -0400

    remove objectManager from productalerts

--- Controller/Module/productalerts.php
+++ Controller/Module/productalerts.php
@@ -17,14 +17,14 @@ class Productalerts extends \Glew\Service\Controller\Module {
         \Magento\Framework\App\Action\Context $context,
         \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory,
         \Glew\Service\Model\Types\ProductAlerts $productAlerts,
-        \Magento\Framework\ObjectManagerInterface $objectManager,
+        /*\Magento\Framework\ObjectManagerInterface $objectManager,*/
         \Glew\Service\Helper\Data $helper
     ) {
 
         $this->resultJsonFactory = $resultJsonFactory;
         $this->productAlerts = $productAlerts;
         $this->helper = $helper;
-        $this->objectManager = $objectManager;
+        /*$this->objectManager = $objectManager;*/
         parent::__construct($context);
         parent::initParams();
 
